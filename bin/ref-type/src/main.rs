57fd7902966180dc9aed6b6cf8070ef760105f67
lvillis <56720445+lvillis@users.noreply.github.com>
2024-01-14 13:21:36 +0800

    fix: actions

---
+++ bin/ref-type/src/main.rs
@@ -0,0 +1,25 @@
+use {regex::Regex, structopt::StructOpt};
+
+#[derive(StructOpt)]
+struct Arguments {
+    #[structopt(long)]
+    reference: String,
+}
+
+fn main() {
+    let arguments = Arguments::from_args();
+
+    let regex = Regex::new("^refs/tags/[[:digit:]]+[.][[:digit:]]+[.][[:digit:]]+$")
+        .expect("Failed to compile release regex");
+
+    let value = if regex.is_match(&arguments.reference) {
+        "release"
+    } else {
+        "other"
+    };
+
+    eprintln!("ref: {}", arguments.reference);
+    eprintln!("value: {value}");
+
+    println!("::set-output name=value::{value}");
+}
